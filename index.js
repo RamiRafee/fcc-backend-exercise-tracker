f660194b62c82da529aa598f0379b4bb1afa66ca
Rami Rafee <kingramy23@gmail.com>
2024-01-26 21:13:09 +0200

    debug onrender

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const port = process.env.PORT || 3000;
 
 
 // Connect to the MongoDB database
+console.log('MongoDB URI:', process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
